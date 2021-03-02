c9d1f0ec21e2f68c3f8c22fa0915c825804a5983
Junaid Oloruntobi <easytobeh@gmail.com>
2021-03-02 14:45:22 +0100

    Created Ammortization Services

--- EntityLayer/Ammortization/AmortizationCalculator.cs
+++ EntityLayer/Ammortization/AmortizationCalculator.cs
@@ -6,5 +6,9 @@ namespace EntityLayer
 {
     class AmortizationCalculator
     {
+        public decimal ReducingBalance(decimal currentBalance)
+        {
+            return currentBalance;
+        }
     }
 }
---
+++ EntityLayer/LoanAmmortization/LoanAmmortization.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.LoansBaseClass
+{
+    class LoanAmmortization
+    {
+        /********DISBURSE LOAN************/
+       // public int AccountId { get; set; }
+        public DateTime DisbursementDate { get; set; }
+        public DateTime FirstRepaymentDate { get; set; }
+        public decimal Payments { get; set; }
+
+        //********** LOAN *********//
+       // public int Id { get; set; }
+        //public int CustomerId { get; set; }
+        public float Tenor { get; set; }
+
+        public int NumberOfPayment { get; set; }
+        public decimal InterestRate { get; set; }
+
+        public string InterestType { get; set; }
+        public DateTime RepaymentStartDate { get; set; }
+
+        //Navigation properties
+
+        public RepayLoan RepayLoan { get; set; }
+        public PaymentRecord PaymentRecord { get; set; }
+        //public LoanType LoanType { get; set; }
+        public PaymentMethod PaymentMethod { get; set; }
+        //public CustomerProfile CustomerProfile { get; set; }
+       // public int CustomerProfileId { get; set; }
+
+        //******REPAY LOAN******************//
+    }
+}
